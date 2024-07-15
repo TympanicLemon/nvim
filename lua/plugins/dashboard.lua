56e852cff32e8170d154251e6359b0d22540223d
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-07-14 23:43:52 -0700

    feat: added dashboard

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,45 @@
+return {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("dashboard").setup({
+            theme = "hyper",
+            config = {
+                project = { enable = false },
+                week_header = {
+                    enable = true,
+                },
+                shortcut = {
+                    {
+                        icon = " ",
+                        icon_hl = "@function",
+                        desc = "Find files",
+                        group = "Label",
+                        action = "Telescope find_files",
+                        key = "f",
+                    },
+                    {
+                        desc = "󰒲 lazy",
+                        group = "Number",
+                        action = "Lazy",
+                        key = "l",
+                    },
+                },
+            },
+        })
+        -- open dashboard after closing lazy
+        if vim.o.filetype == "lazy" then
+            vim.api.nvim_create_autocmd("WinClosed", {
+                pattern = tostring(vim.api.nvim_get_current_win()),
+                once = true,
+                callback = function()
+                    vim.schedule(function()
+                        vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
+                    end)
+                end,
+            })
+        end
+
+    end
+}
