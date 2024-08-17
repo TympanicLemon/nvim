9f4cc3a6823ea2367ad8b5d13675a1c028812fc1
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-08-17 13:01:17 -0700

    added lsp support :D

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,26 @@
+return {
+    { "neovim/nvim-lspconfig" },
+
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "mason.nvim" },
+        config = function()
+            require("mason-lspconfig").setup {
+                ensure_installed = { "lua_ls", "clangd" },
+            }
+
+            require("mason-lspconfig").setup_handlers({
+                function(server_name)
+                    require("lspconfig")[server_name].setup({})
+                end
+            })
+        end,
+    }
+}
