5e1e358a6689ffc427e48d6bfd4930249b348691
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-08-17 14:13:46 -0700

    added conform and mason tool installer :D

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,15 +12,31 @@ return {
         "williamboman/mason-lspconfig.nvim",
         dependencies = { "mason.nvim" },
         config = function()
-            require("mason-lspconfig").setup {
-                ensure_installed = { "lua_ls", "clangd" },
-            }
-
             require("mason-lspconfig").setup_handlers({
                 function(server_name)
                     require("lspconfig")[server_name].setup({})
                 end
             })
         end,
-    }
+    },
+
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        dependencies = { "mason.nvim", "mason-lspconfig.nvim" },
+        config = function()
+            require('mason-tool-installer').setup({
+                ensure_installed = {
+                    "lua-language-server",
+                    "clangd",
+                    "stylua",
+                    "black", -- Python formatter
+                    "isort", -- Python import sorter
+                },
+
+                integrations = {
+                    ["mason-lspconfig"] = true,
+                },
+            })
+        end,
+    },
 }
