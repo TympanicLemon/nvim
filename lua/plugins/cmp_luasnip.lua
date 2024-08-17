4e604fa6fad0d214d037cf39d2c55094eaba131f
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-08-17 15:49:32 -0700

    aded cmp support for the path

--- lua/plugins/cmp_luasnip.lua
+++ lua/plugins/cmp_luasnip.lua
@@ -1,6 +1,6 @@
 return {
     {
-        "hrsh7th/cmp-nvim-lsp"
+        "hrsh7th/cmp-nvim-lsp",
     },
 
     {
@@ -13,6 +13,10 @@ return {
 
     {
         "hrsh7th/nvim-cmp",
+        dependencies = {
+            "L3MON4D3/LuaSnip",
+            "hrsh7th/cmp-path",
+        },
         config = function()
             local cmp = require("cmp")
             local luasnip = require("luasnip")
@@ -29,16 +33,16 @@ return {
                     documentation = cmp.config.window.bordered(),
                 },
                 mapping = {
-                    ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-                    ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+                    ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+                    ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
                     ["<Up>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
                     ["<Down>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm { select = false },
+                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
                     ["<Tab>"] = cmp.mapping(function(fallback)
                         if cmp.visible() then
-                            cmp.confirm { select = true }
+                            cmp.confirm({ select = true })
                         elseif luasnip.jumpable(1) then
                             luasnip.jump(1)
                         else
@@ -48,12 +52,12 @@ return {
                 },
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
-                    { name = "luasnip" }, -- For luasnip users.
+                    { name = "luasnip" },
+                    { name = "path" },
                 }, {
                     { name = "buffer" },
                 }),
             })
         end,
-        dependencies = "L3MON4D3/LuaSnip",
     },
 }
