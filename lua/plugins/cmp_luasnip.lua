b3e83b2694aeb56d5ef2159146dd0e45f0ee23da
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-08-17 13:39:20 -0700

    added small keymapping changes to nvim_cmp

--- lua/plugins/cmp_luasnip.lua
+++ lua/plugins/cmp_luasnip.lua
@@ -15,6 +15,7 @@ return {
         "hrsh7th/nvim-cmp",
         config = function()
             local cmp = require("cmp")
+            local luasnip = require("luasnip")
             require("luasnip.loaders.from_vscode").lazy_load()
 
             cmp.setup({
@@ -27,20 +28,32 @@ return {
                     completion = cmp.config.window.bordered(),
                     documentation = cmp.config.window.bordered(),
                 },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                mapping = {
+                    ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+                    ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+                    ["<Up>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+                    ["<Down>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                }),
+                    ["<CR>"] = cmp.mapping.confirm { select = false },
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.confirm { select = true }
+                        elseif luasnip.jumpable(1) then
+                            luasnip.jump(1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                },
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
-                    { name = "luasnip" },
+                    { name = "luasnip" }, -- For luasnip users.
                 }, {
                     { name = "buffer" },
                 }),
             })
         end,
+        dependencies = "L3MON4D3/LuaSnip",
     },
 }
