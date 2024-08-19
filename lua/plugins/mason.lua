55d2cfa0e426632c73ce61a204f5f3f41cdc60d1
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-08-18 18:12:44 -0700

    moved mason plugins to its own file, "mason.lua"

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,48 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "mason.nvim" },
+        config = function()
+            require("mason-lspconfig").setup_handlers({
+                function(server_name)
+                    require("lspconfig")[server_name].setup({})
+                end,
+            })
+        end,
+    },
+
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        dependencies = { "mason.nvim", "mason-lspconfig.nvim" },
+        config = function()
+            require("mason-tool-installer").setup({
+                ensure_installed = {
+                    -- LSPs
+                    "lua-language-server",
+                    "clangd",
+
+                    -- Formatters
+                    "stylua",
+                    "black",
+                    "isort",
+                    "clang-format",
+
+                    -- Debuggers
+                    "codelldb",
+                },
+
+                integrations = {
+                    ["mason-lspconfig"] = true,
+                    ["mason-nvim-dap"] = true,
+                },
+            })
+        end,
+    },
+}
