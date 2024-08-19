f4dc8dda41f8ca5a03c4435a2c9e24e45df71367
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-08-18 20:31:48 -0700

    added nvim-highlight-colors

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,5 +1,10 @@
 return {
     {
+        "brenoprata10/nvim-highlight-colors",
+        opts = {},
+    },
+
+    {
         "sainnhe/gruvbox-material",
         lazy = false,
         priority = 1000,
