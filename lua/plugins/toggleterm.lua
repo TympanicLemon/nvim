d75b0e39242820e343c5bfbda08e816332235402
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-08-18 20:18:03 -0700

    got rid of toggleterm, sticking to ghostty for terminal stuff

--- lua/plugins/toggleterm.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = function()
-        require("toggleterm").setup({})
-        vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm size=16 direction=horizontal<cr>")
-    end,
-}
