a10d404f3c155cfbac2f471d8cc028fdb82eb1eb
Daniel Zapata <danielz@Daniels-MBP-2.attlocal.net>
2024-08-20 21:50:05 -0700

    added some more keymaps, and made sure keymaps are the same syntax wise

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,55 +1,47 @@
 local map = vim.keymap.set
 
--- move between splits/windows
-map("n", "<C-h>", "<C-w>h")
-map("n", "<C-j>", "<C-w>j")
-map("n", "<C-k>", "<C-w>k")
-map("n", "<C-l>", "<C-w>l")
+-- Window Navigation
+map("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
+map("n", "<C-j>", "<C-w>j", { desc = "Move to below split" })
+map("n", "<C-k>", "<C-w>k", { desc = "Move to above split" })
+map("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
 
--- quit and save
-map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>")
-map("n", "<leader>q", "<CMD>qa<CR>")
+-- Buffer Navigation
+map("n", "<S-h>", "<CMD>bprevious<CR>", { desc = "Previous buffer" })
+map("n", "<S-l>", "<CMD>bnext<CR>", { desc = "Next buffer" })
 
--- resize lines
-map("n", "<C-Up>", "<cmd>resize +2<cr>")
-map("n", "<C-Down>", "<cmd>resize -2<cr>")
-map("n", "<C-Left>", "<cmd>vertical resize -2<cr>")
-map("n", "<C-Right>", "<cmd>vertical resize +2<cr>")
+-- Resizing Splits
+map("n", "<C-Up>", "<CMD>resize +2<CR>", { desc = "Increase height" })
+map("n", "<C-Down>", "<CMD>resize -2<CR>", { desc = "Decrease height" })
+map("n", "<C-Left>", "<CMD>vertical resize -2<CR>", { desc = "Decrease width" })
+map("n", "<C-Right>", "<CMD>vertical resize +2<CR>", { desc = "Increase width" })
 
--- Move Lines
-map("n", "<A-j>", "<cmd>m .+1<cr>==")
-map("n", "<A-k>", "<cmd>m .-2<cr>==")
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi")
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi")
-map("v", "<A-j>", ":m '>+1<cr>gv=gv")
-map("v", "<A-k>", ":m '<-2<cr>gv=gv")
+-- Line Movement
+map("n", "<A-j>", "<CMD>m .+1<CR>==", { desc = "Move line down" })
+map("n", "<A-k>", "<CMD>m .-2<CR>==", { desc = "Move line up" })
+map("i", "<A-j>", "<esc><CMD>m .+1<CR>==gi", { desc = "Move line down in insert mode" })
+map("i", "<A-k>", "<esc><CMD>m .-2<CR>==gi", { desc = "Move line up in insert mode" })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
--- Move between buffers
-map("n", "<S-h>", "<cmd>bprevious<cr>")
-map("n", "<S-l>", "<cmd>bnext<cr>")
+-- Editing Enhancements
+map({ "i", "x", "n", "s" }, "<C-s>", "<CMD>w<CR><esc>", { desc = "Save file" })
+map("n", "<leader>q", "<CMD>qa<CR>", { desc = "Quit all" })
+map("i", "<S-CR>", "<ESC>o", { desc = "Insert new line below" })
+map("i", ",", ",<c-g>u", { desc = "Add undo break after comma" })
+map("i", ".", ".<c-g>u", { desc = "Add undo break after period" })
+map("i", ";", ";<c-g>u", { desc = "Add undo break after semicolon" })
+map("v", "<", "<gv", { desc = "Better indenting" })
+map("v", ">", ">gv", { desc = "Better indenting" })
 
--- Clear search with <esc>
-map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>")
+-- Centering Scroll
+map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+map("n", "n", "nzzzv", { desc = "Keep cursor centered after search" })
+map("n", "N", "Nzzzv", { desc = "Keep cursor centered after reverse search" })
 
--- Insert a new line below
-map("i", "<S-CR>", "<ESC>o")
-
--- Add undo break-points
-map("i", ",", ",<c-g>u")
-map("i", ".", ".<c-g>u")
-map("i", ";", ";<c-g>u")
-
--- better indenting
-map("v", "<", "<gv")
-map("v", ">", ">gv")
-
--- Enter normal mode in terminal
-map("t", "<esc><esc>", "<c-\\><c-n>")
-
--- Open Lazy
-map("n", "<leader>L", "<cmd>Lazy<cr>")
-
-map("n", "<C-u>", "<C-u>zz", { desc = "scroll up and center" })
-map("n", "<C-d>", "<C-d>zz", { desc = "scroll down and center" })
-map("n", "n", "nzzzv", { desc = "keep cursor centered" })
-map("n", "N", "Nzzzv", { desc = "keep cursor centered" })
+-- Miscellaneous
+map({ "i", "n" }, "<esc>", "<CMD>noh<CR><esc>", { desc = "Clear search highlights" })
+map("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter normal mode in terminal" })
+map("n", "<leader>L", "<CMD>Lazy<CR>", { desc = "Open Lazy" })
+map("n", "<leader>ya", "ggyG", { desc = "Yank all" })
